0987fcee6d045f4a09f7c0a625af53b5b3b92a70
leezzx <79442805+leezzx@users.noreply.github.com>
2021-04-08 14:20:27 +0900

    Add java file [6]

---
+++ Add2.java
@@ -0,0 +1,31 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/add2")
+public class Add2 extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset = UTF-8");
+		
+		String[] num_ = request.getParameterValues("num"); // 입력값을 배열로 받기 위한 구문
+		
+		int result = 0;
+		
+		for(int i = 0; i < num_.length; i++) {
+			int num = Integer.parseInt(num_[i]);
+			result += num;
+		}
+			
+		response.getWriter().printf("result is %d\n", result);
+		
+	}
+
+}
